66121e5eaf08236414c57b0bb7fb3a08cf260ade
Ivan Alexis Crisanto Montero <iacrismo16@gmail.com>
2025-11-03 14:51:26 +0000

    porque usuario se queda ahi, tiene que estar abajo

--- src/components/dashboard/sidebar.tsx
+++ src/components/dashboard/sidebar.tsx
@@ -38,8 +38,8 @@ export default function Sidebar() {
             <span className="">Mewing</span>
           </Link>
         </div>
-        <div className="flex-1 overflow-auto py-2">
-          <ScrollArea className="h-full">
+        <div className="flex-1 overflow-auto">
+          <ScrollArea className="h-full py-2">
             <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
               {navItems.map(({ href, icon: Icon, label }) => (
                 <Link
