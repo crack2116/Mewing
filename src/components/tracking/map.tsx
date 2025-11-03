3dfe302a16a2bca73b39895395fd4dad3ef5c494
Ivan Alexis Crisanto Montero <iacrismo16@gmail.com>
2025-11-03 01:12:42 +0000

    Try fixing this error: `Runtime Error: Error: Map container is already i

--- src/components/tracking/map.tsx
+++ src/components/tracking/map.tsx
@@ -2,10 +2,19 @@
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import { Icon } from 'leaflet';
+import L, { Icon } from 'leaflet';
 import { Button } from '@/components/ui/button';
 import { Pause } from 'lucide-react';
 import type { ActiveVehicle } from '@/lib/types';
+
+// Fix for default icon path issue with webpack
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+});
+
 
 const customIcon = new Icon({
   iconUrl: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI0OCIgaGVpZ2h0PSI0OCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiNmZmZmZmYiIHN0cm9rZS13aWR0aD0iMSIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS10cnVjayI+PGcgc3Ryb2tlPSIjZmZmZmZmIiBmaWxsPSIjMjU2M2ViIj48cGF0aCBkPSJNNC41IDE1LjVDMy42NyAxNSAyIDE0LjMzIDIuNSA4LjVMMyA2aDEyLjQ3Yy41MiAwIC45OC4yOCAxLjI1LjcxTDIwIDEyVjE5SDJhMiAyIDAgMCAxIDAtNHoiLz48cGF0aCBkPSJtMTYgMyAzLjQxIDQuNTkiLz48Y2lyY2xlIGN4PSI2IiBjeT0iMTkiIHI9IjIiLz48Y2lyY2xlIGN4PSIxNyIgY3k9IjE5IiByPSIyIi8+PC9nPjwvc3ZnPg==',
@@ -24,6 +33,7 @@ export default function Map({ vehicles }: MapProps) {
         zoom={13}
         scrollWheelZoom={false}
         className="h-full w-full"
+        whenCreated={map => map.invalidateSize()}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
