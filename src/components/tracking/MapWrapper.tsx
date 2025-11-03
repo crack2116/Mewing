a1e70a28a027e28f9360738b32c0ccbf60226ee9
Ivan Alexis Crisanto Montero <iacrismo16@gmail.com>
2025-11-03 16:48:25 +0000

    no veo cambios sigue lo mismo , cambialo

--- src/components/tracking/MapWrapper.tsx
+++ src/components/tracking/MapWrapper.tsx
@@ -8,7 +8,7 @@ import { Pause, Play } from 'lucide-react';
 import type { ActiveVehicle } from '@/lib/types';
 
 const customIcon = new Icon({
-    iconUrl: `data:image/svg+xml;base64,${btoa('<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 32 46"><path fill="#022a4d" d="M15.5 45.75c.31.32.73.5 1.16.5.43 0 .85-.18 1.16-.5C18.14 45.42 32 30.01 32 16.5 32 7.39 24.84 0 16 0S0 7.39 0 16.5C0 30.01 13.86 45.42 15.5 45.75z"/><path fill="#1d63ff" d="M16 43c.4 0 .78-.17 1.05-.45C17.34 42.25 30 28.18 30 16A14 14 0 1 0 2 16c0 12.18 12.66 26.25 12.95 26.55c.27.28.65.45 1.05.45z"/><path fill="#fff" d="M22.4 17.3h-2.5v-2c0-1-1-2-2-2h-3.8c-1 0-2 1-2 2v2H9.6c-1 0-1.5.5-1.5 1.5v5c0 1 .5 1.5 1.5 1.5h.7c.3-1.2 1.4-2 2.7-2s2.4.8 2.7 2h3.4c.3-1.2 1.4-2 2.7-2s2.4.8 2.7 2h.7c1 0 1.5-.5 1.5-1.5v-5c0-1-.5-1.5-1.5-1.5zM13 21a1 1 0 1 1-2 0a1 1 0 0 1 2 0zm6 0a1 1 0 1 1-2 0a1 1 0 0 1 2 0z"/></svg>')}`,
+    iconUrl: '/icons/truck-marker.svg',
     iconSize: [40, 60],
     iconAnchor: [20, 60],
     popupAnchor: [0, -60],
